c87e6b1f81c8013a0ff9af66cd7ab6d4b4660b04
MilkyLatte01 <wrawprachkraithip@gmail.com>
2023-02-09 15:00:31 +0700

    Practice01 9-2-2023

--- OOP_Program/src/Cylinder.java
+++ OOP_Program/src/Cylinder.java
@@ -2,13 +2,17 @@ public class Cylinder extends Circle{
 
     private double height;
     public Cylinder(double radius, double height) {
+    	
         super(radius, null);
         this.height = height;
+        
     }
 
     public double getHeight(){
         return this.height;
+   
     }
+    
     public double getVolume(){
         return super.getArea() * getHeight();
     }
